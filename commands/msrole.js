9c44622dc3003b9ce467cd390b7effbf6aaafeb6
Fontesie <57833419+Fontesie@users.noreply.github.com>
2023-05-16 00:10:51 +0200

    V1.1

--- commands/msrole.js
+++ commands/msrole.js
@@ -2,8 +2,8 @@ module.exports.run = async (client, message, args) => {
     const config = require("../config.json");
     if (message.author.id == config.idowner) { } else return;
     message.delete();
-
-    for (let i = 0; i < role1; i++) {
+    
+    for (let i = 0; i < config.role1; i++) {
         message.guild.roles.create({
             color: 'RED',
             name: 'AmOnG uS',
